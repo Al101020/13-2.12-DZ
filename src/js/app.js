505072d0248b6635ff14bf634abea3606542952a
Al101020 <alex.vl.m@yandex.ru>
2025-08-08 11:25:57 +1000

    commit-1

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+if (window.Worker) {
+  console.log('worker есть');
+  // //   const worker = new Worker('../worker.js');
+  // const worker = new Worker('http://localhost:8082/src/worker.js');
+  // console.log(worker);
+}
