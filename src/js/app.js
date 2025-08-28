836e55d841f4fa275e5e3b2aa15a57a8bc404433
Al101020 <alex.vl.m@yandex.ru>
2025-08-28 17:37:44 +1000

    commit-01.09

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,36 @@
 import '../component/movieNews/movieNews';
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js', { scope: './' })
-    .then((reg) => {
-      // регистрация сработала
-      console.log(`Registration succeeded. Scope is ${reg.scope}`);
-    }).catch((error) => {
-      // регистрация прошла неудачно
-      console.log(`Registration failed with ${error}`);
-    });
-}
+if ('serviceWorker' in navigator) { 
+        navigator.serviceWorker.register('./service-worker.js', { scope: './' })
+          .then((reg) => {
+            // регистрация сработала
+            console.log('Registration succeeded. Scope is ' + reg.scope);
+          }).catch((error) => {
+            // регистрация прошла неудачно
+            console.log('Registration failed with ' + error);
+          });
+      }
+
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('service-worker.js', { scope: './' })
+//     .then((reg) => {
+//       // регистрация сработала
+//       console.log(`Registration succeeded. Scope is ${reg.scope}`);
+//     }).catch((error) => {
+//       // регистрация прошла неудачно
+//       console.log(`Registration failed with ${error}`);
+//     });
+// }
+
+
+
+
+
+
+
+
+
+
 
 // if ('serviceWorker' in navigator) {
 //   console.log('SW в navigator есть');
--- src/service-worker.js
+++ src/service-worker.js
@@ -14,4 +14,5 @@ console.log('service-worker.js');
 //           // регистрация прошла неудачно
 //     console.log('Registration failed with ' + error);
 //   });
-// }
+// } 
+
